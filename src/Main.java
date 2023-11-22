fe729e709f6e19c71e5968448f4fe52ddd6d76d1
carsonnannini <carsonnannini@arizona.edu>
2023-11-21 18:40:53 -0700

    main fix

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,35 @@
-public class Main{
-
+/**
+ * file: main.java
+ * @author Carson Nannini, Michael Williams
+ * Purpose: This program proves that the internal structure of our Fibonacci 
+ * Heap by printing the root list and nodes removed on our Fibonacci Heap. 
+ *
+ */
+public class main {
     public static void main(String[] args) {
-        System.out.println("Running Test Case 1: Basic Functionality");
-        Test1.testCaseBasicFunctionality();
-        System.out.println();
-
-        System.out.println("Running Test Case 2: Decreasing Priority");
-        Test1.testCaseDecreasePriority();
-        System.out.println();
-
-        System.out.println("Running Test Case 3: Edge Cases");
-        Test1.testCaseEdgeCases();
-        System.out.println();
-
-        System.out.println("Running Test Case 4: Emptying the Queue");
-        Test1.testCaseEmptyQueue();
-        System.out.println();
-
-        System.out.println("Running Test Case 5: Large Number of Patients");
-        Test1.testCaseLargeNumberOfPatients();
-        System.out.println();
-
-        System.out.println("Running Test Case 6: Should print patients in order of priority");
-        Test1.testCase6();
-        System.out.println();
+    	// insert
+        HospitalQueue q = new HospitalQueue();
+        q.addPatient("Tim", 2);
+        q.addPatient("Jim", 6);
+        q.addPatient("Kim", 3);
+        q.addPatient("Al", 5);
+        q.addPatient("Allen", 7);
+        
+        // print and dequeue
+        System.out.println(q.toString());
+        FibNode patient = q.dequeue();
+        System.out.println("Patient removed: " + patient.name + " " + patient.priority);
+        System.out.println(q.toString());
+        patient = q.dequeue();
+        System.out.println("Patient removed: " + patient.name + " " + patient.priority);
+        System.out.println(q.toString());
+        patient = q.dequeue();
+        System.out.println("Patient removed: " + patient.name + " " + patient.priority);
+        System.out.println(q.toString());
+        patient = q.dequeue();
+        System.out.println("Patient removed: " + patient.name + " " + patient.priority);
+        System.out.println(q.toString());
+        patient = q.dequeue();
+        System.out.println("Patient removed: " + patient.name + " " + patient.priority);
     }
 }
